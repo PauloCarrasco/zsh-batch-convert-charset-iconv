649a18385709693aa59019756bb6919e68ddd219
Paulo Carrasco <carrasco@Paulos-Mac-mini.local>
2021-10-14 16:20:44 +0100

    first commit

---
+++ convert-charset.sh
@@ -0,0 +1,54 @@
+#!/bin/zsh
+
+PWD="$(pwd)"
+BACKUP="backup"
+SRC_ENCODING="UTF-8"
+TGT_ENCODING=$1
+EXT=$2
+
+echo ""
+
+if [ -z $TGT_ENCODING ]; then
+
+	TGT_ENCODING="UTF-8"
+fi
+
+if [ -z $EXT ]; then
+	echo ""
+	echo "Encoding or extension parameters required."
+	echo ""
+	echo "Usage: convert-charset <target encoding> <extension> "
+	echo "   ex: convert-charset UTF-8 txt"
+	echo ""
+	exit
+fi
+
+for file in $PWD/*.$EXT
+do
+	if [[ $file =~ \.$EXT$ ]]; then
+
+		SRC_ENCODING=$(file -b --mime-encoding "$file")
+
+		echo "[IN > OUT] $SRC_ENCODING > $TGT_ENCODING"
+		echo "[ENCODING] iconv -f $SRC_ENCODING -t UTF8 $file"
+
+		mv $file $file.bak
+		iconv -f $SRC_ENCODING -t $TGT_ENCODING  $file.bak > $file
+
+		echo "[ENCODED ] $(file -I "$file")"
+	else
+		echo "found $file"
+		echo "is not a $EXT file"
+		echo "Skip!"
+	fi
+done
+
+if [ -w $BACKUP ]; then
+    	echo "[BACKUP  ] DONE"
+	else
+		mkdir $BACKUP
+fi
+
+mv *.bak backup/.
+
+echo ""
